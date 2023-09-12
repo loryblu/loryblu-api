040726ad7f059adddf80b51bc436cb908cc50904
viniciuscosmome <48590313+viniciuscosmome@users.noreply.github.com>
2023-09-12 16:22:28 -0300

    test: create reset password tests

--- src/modules/parent/parent.service.spec.ts
+++ src/modules/parent/parent.service.spec.ts
@@ -10,6 +10,10 @@ describe('ParentService unit test', () => {
 
   const parentRepositoryMock = {
     saveCredentialParentAndChildrenProps: jest.fn(),
+    getCredentialIdByEmail: jest
+      .fn()
+      .mockReturnValue(stubs.getCredentialOutput),
+    savePasswordResetInformation: jest.fn(),
   };
 
   beforeEach(async () => {
@@ -48,6 +52,37 @@ describe('ParentService unit test', () => {
         );
         expect(actual).toBeInstanceOf(BadRequestException);
       }
+    });
+  });
+
+  describe('Reset password', () => {
+    it('Happy path - should return { url: string; fullname: string }', async () => {
+      const actual = await service.createTokenToResetPassword(
+        stubs.resetPasswordInput,
+      );
+
+      expect(actual).toBeInstanceOf(Object);
+
+      if (actual) {
+        expect(actual.url).toBeDefined();
+        expect(actual.url).toMatch(
+          /^loryblu:\/\/password_recovery\/\?r_token%3D([a-zA-Z0-9_-]+)%26expires_in%3D(([0-9]{1,6})(T|-|(%3A)|\.)?){6}([0-9]{3})Z$/,
+        );
+        expect(actual.fullname).toBeDefined();
+      }
+    });
+
+    it('Unhappy path - should return void', async () => {
+      jest
+        .spyOn(parentRepositoryMock, 'getCredentialIdByEmail')
+        .mockReturnValueOnce(null);
+
+      const actual = await service.createTokenToResetPassword(
+        stubs.resetPasswordInput,
+      );
+
+      expect(actual).not.toBeInstanceOf(Object);
+      expect(actual).toBeUndefined();
     });
   });
 });
