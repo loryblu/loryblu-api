87aa21da16c1d7aba760d03aa2604fe24454a867
viniciuscosmome <48590313+viniciuscosmome@users.noreply.github.com>
2023-09-19 17:43:11 -0300

    chore(sec): configure cors and helmet

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { SwaggerModule } from '@nestjs/swagger';
+import helmet from 'helmet';
 
 import { swaggerDocumentConfig } from './globals/swagger';
+import { corsOptionsConfig } from './globals/cors';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.use(helmet());
+  app.enableCors(corsOptionsConfig);
   app.useGlobalPipes(new ValidationPipe());
 
   const document = SwaggerModule.createDocument(app, swaggerDocumentConfig);
