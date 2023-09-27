09a89d8fd1bfd813c1e0b7dd2f848f19f4da7d44
viniciuscosmome <48590313+viniciuscosmome@users.noreply.github.com>
2023-09-26 22:03:54 -0300

    refactor: updates error responses

--- src/globals/constants.ts
+++ src/globals/constants.ts
@@ -4,7 +4,6 @@ export const appLicense = process.env.npm_package_license;
 
 export const fullnameRegExp = /^[a-zÀ-ÿ ]+$/i;
 export const recoveryTokenRegExp = /^[a-zA-Z0-9_-]+$/;
-export const childrenBirthDateExample = '2009-02-28';
 
 export const isDevelopmentEnv = () => process.env.NODE_ENV === 'development';
 export const isHomologationEnv = () => process.env.NODE_ENV === 'homologation';
--- src/globals/errors.ts
+++ src/globals/errors.ts
@@ -3,44 +3,7 @@ import {
   InternalServerErrorException,
 } from '@nestjs/common';
 import { Prisma } from '@prisma/client';
-import type { ValidationErrorMessagesProps } from './entity';
 import { isDevelopmentEnv } from './constants';
-
-export const validationErrorMessages: ValidationErrorMessagesProps = {
-  emptyField: (args) => {
-    return `O ${args.property} não pode estar vazio.`;
-  },
-  booleanField: (args) => {
-    return `O ${args.property} deve ser booleano.`;
-  },
-  stringField: (args) => {
-    return `O ${args.property} deve ser do tipo texto.`;
-  },
-  minLength: (args) => {
-    return `O ${args.property} deve conter no mínimo ${args.constraints[0]} caracteres.`;
-  },
-  maxLength: (args) => {
-    return `O ${args.property} deve conter no máximo ${args.constraints[1]} caracteres.`;
-  },
-  fullnameField: (args) => {
-    return `O ${args.property} deve conter apenas letras e espaço em branco.`;
-  },
-  emailPattern: () => {
-    return 'O e-mail informado deve ter um formato válido. ex: suenome@exemplo.com';
-  },
-  passwordPattern: () => {
-    return 'A senha deve conter no mínimo uma letra maiúscula, uma letra minúscula, um número, um símbolo e no mínimo 8 caracteres.';
-  },
-  birthDatePattern: () => {
-    return `A data de nascimento informada deve ser do tipo texto e ser neste formato: YYYY-MM-DD`;
-  },
-  genderPattern: (args) => {
-    return `O sexo deve ser [${args.constraints[1]}].`;
-  },
-  pattern: (args) => {
-    return `O ${args.property} está em um formato inválido.`;
-  },
-};
 
 export function prismaKnownRequestErrors(
   error: Prisma.PrismaClientKnownRequestError,
