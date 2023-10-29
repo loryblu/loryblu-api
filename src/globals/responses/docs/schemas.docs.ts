0b7601f5d368e8af9d0db724886437a127b0b9d3
viniciuscosmome <48590313+viniciuscosmome@users.noreply.github.com>
2023-10-29 01:56:56 -0300

    docs: changes responses schema

--- src/globals/responses/docs/schemas.docs.ts
+++ src/globals/responses/docs/schemas.docs.ts
@@ -1,50 +1,40 @@
-import { DocSchemas } from 'src/globals/entity';
+import type { SchemaObject } from '@nestjs/swagger/dist/interfaces/open-api-spec.interface';
 
-function successSchema() {
-  return {
-    type: 'object',
-    properties: {
-      message: {
-        type: 'string',
-      },
-      data: {
-        type: 'object',
-        nullable: true,
-      },
+const successSchema: SchemaObject = {
+  type: 'object',
+  properties: {
+    message: {
+      type: 'string',
     },
-  };
-}
+    data: {
+      type: 'object',
+      nullable: true,
+    },
+  },
+};
 
-function errorSchema() {
-  return {
-    type: 'object',
-    properties: {
-      details: {
-        type: 'array',
-        items: {
-          type: 'object',
-          properties: {
-            property: {
-              type: 'string',
-              nullable: true,
-            },
-            message: {
-              type: 'string',
-            },
+const errorSchema: SchemaObject = {
+  type: 'object',
+  properties: {
+    details: {
+      type: 'array',
+      items: {
+        type: 'object',
+        properties: {
+          property: {
+            type: 'string',
+            nullable: true,
+          },
+          message: {
+            type: 'string',
           },
         },
       },
     },
-  };
-}
+  },
+};
 
-export const docSchemas: DocSchemas = {
-  $200: successSchema(),
-  $201: successSchema(),
-  $202: successSchema(),
-  $400: errorSchema(),
-  $401: errorSchema(),
-  $403: errorSchema(),
-  $422: errorSchema(),
-  $500: errorSchema(),
+export const docSchemas = {
+  success: successSchema,
+  error: errorSchema,
 };
