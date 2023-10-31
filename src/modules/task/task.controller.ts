c35ceb03400fbc5e69f532ffe7e3e057be0fa02d
viniciuscosmome <48590313+viniciuscosmome@users.noreply.github.com>
2023-10-31 20:19:19 -0300

    chore: use parent id from payload

--- src/modules/task/task.controller.ts
+++ src/modules/task/task.controller.ts
@@ -1,14 +1,24 @@
-import { Body, Controller, HttpCode, Post } from '@nestjs/common';
-import { TaskService } from './task.service';
+import {
+  Body,
+  Controller,
+  HttpCode,
+  Post,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
 import { ApiResponse, ApiTags } from '@nestjs/swagger';
 import { responses } from 'src/globals/responses/docs';
+import { TaskService } from './task.service';
 import { TaskCreateDto } from './task.dto';
+import { AuthorizationGuard, RequestToken } from 'src/guard';
 
+@UseGuards(AuthorizationGuard)
 @Controller('/task')
 @ApiTags('Tasks')
 export class TaskController {
   constructor(private service: TaskService) {}
 
+  @RequestToken({ type: 'access', role: 'user' })
   @Post()
   @HttpCode(201)
   @ApiResponse(responses.ok)
@@ -17,10 +27,11 @@ export class TaskController {
   @ApiResponse(responses.forbidden)
   @ApiResponse(responses.unprocessable)
   @ApiResponse(responses.internalError)
-  async create(@Body() input: TaskCreateDto) {
+  async create(@Body() input: TaskCreateDto, @Req() request: Request) {
+    const sessionInfo = request['session.payload'];
+
     await this.service.processNewTaskData({
-      // ! Pegar o id da conta ativa
-      parentId: 'd96fd284-3995-4a71-8f49-e8e99342b6d4',
+      parentId: sessionInfo.pid as string,
       ...input,
     });
 
--- src/modules/task/task.module.ts
+++ src/modules/task/task.module.ts
@@ -3,9 +3,10 @@ import { PrismaModule } from 'src/prisma/prisma.module';
 import { TaskController } from './task.controller';
 import { TaskService } from './task.service';
 import { TaskRepository } from './task.repository';
+import jwtModule from 'src/lib/jwt';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, jwtModule],
   controllers: [TaskController],
   providers: [TaskService, TaskRepository],
 })
