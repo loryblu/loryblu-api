9dacfc7ec77d9f5b0968b9a64fb8d36e9c68671e
viniciuscosmome <48590313+viniciuscosmome@users.noreply.github.com>
2023-11-01 19:29:57 -0300

    docs(swagger): authorization tokens

--- src/globals/swagger.ts
+++ src/globals/swagger.ts
@@ -10,8 +10,40 @@ export const swaggerDocumentConfig = new DocumentBuilder()
     `${appLicense} License`,
     'https://github.com/loryblu/loryblu-api/blob/main/LICENSE',
   )
+  .addBearerAuth(
+    {
+      type: 'http',
+      scheme: 'bearer',
+      name: 'authorization',
+      in: 'header',
+      bearerFormat: 'JWT',
+      description: 'Use `accessToken`, ele é obtido no login.',
+    },
+    'Token de Acesso',
+  )
+  .addBearerAuth(
+    {
+      type: 'http',
+      scheme: 'bearer',
+      name: 'authorization',
+      in: 'header',
+      bearerFormat: 'JWT',
+      description: 'Use `refreshToken`, ele é obtido no login.',
+    },
+    'Token de atualização de sessão',
+  )
+  .addBearerAuth(
+    {
+      type: 'http',
+      scheme: 'bearer',
+      name: 'authorization',
+      in: 'header',
+      bearerFormat: 'JWT',
+      description:
+        'Use `recoveryToken`, ele é obtido no e-mail enviado ao pedir a redefinição de senha.',
+    },
+    'Token de redefinição de senha',
+  )
   .build();
 
-swaggerDocumentConfig.components = {
-  schemas: docSchemas,
-};
+swaggerDocumentConfig.components.schemas = docSchemas;
