75c26abe1ce88cb7dcceec0a93485c5fd907bf90
Lauane Gonçalves <lauane.gcsilva@gmail.com>
2023-11-11 19:58:17 -0300

    refactor: change MailerModule local

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,27 +2,8 @@ import { Module } from '@nestjs/common';
 import configModule from 'src/globals/constants';
 
 import { AccountModule, TaskModule } from './modules';
-import { MailerModule } from '@nestjs-modules/mailer';
 
 @Module({
-  imports: [
-    configModule,
-    TaskModule,
-    AccountModule,
-    MailerModule.forRoot({
-      transport: {
-        port: Number(process.env.MAIL_PORT),
-        host: process.env.MAIL_HOST,
-        secure: process.env.MAIL_PORT === '465' ? true : false,
-        auth: {
-          user: process.env.MAIL_USER,
-          pass: process.env.MAIL_PASS,
-        },
-      },
-      defaults: {
-        from: `${process.env.MAIL_NAME} <${process.env.MAIL_FROM}>`,
-      },
-    }),
-  ],
+  imports: [configModule, TaskModule, AccountModule],
 })
 export class AppModule {}
--- src/modules/mail/mail.module.ts
+++ src/modules/mail/mail.module.ts
@@ -1,7 +1,24 @@
 import { Module } from '@nestjs/common';
 import { MailService } from './mail.service';
+import { MailerModule } from '@nestjs-modules/mailer';
 
 @Module({
+  imports: [
+    MailerModule.forRoot({
+      transport: {
+        port: Number(process.env.MAIL_PORT),
+        host: process.env.MAIL_HOST,
+        secure: process.env.MAIL_PORT === '465' ? true : false,
+        auth: {
+          user: process.env.MAIL_USER,
+          pass: process.env.MAIL_PASS,
+        },
+      },
+      defaults: {
+        from: `${process.env.MAIL_NAME} <${process.env.MAIL_FROM}>`,
+      },
+    }),
+  ],
   providers: [MailService],
   exports: [MailService],
 })
