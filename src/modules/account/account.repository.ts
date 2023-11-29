41cb2dd8528e81e1d3100939d3f1b9a5f6e5b2b0
Daiane Bolzan <dainha85@gmail.com>
2023-11-28 23:28:52 -0300

    fix: removing

--- src/modules/account/account.repository.ts
+++ src/modules/account/account.repository.ts
@@ -97,7 +97,6 @@ export class AccountRepository {
     return response;
   }
 
-
   async getCredentialIdByRecoveryToken(
     input: getCredentialIdByRecoveryTokenInput,
   ): Promise<getCredentialIdByRecoveryTokenOutout> {
