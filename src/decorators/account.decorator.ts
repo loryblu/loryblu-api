dc169037d6aabfc2b1d9d3b9f382f157e57e5648
Daiane Bolzan <101154455+Daaaiii@users.noreply.github.com>
2024-02-20 20:07:57 -0300

    Update src/decorators/account.decorator.ts
    Co-authored-by: Cosmo <48590313+viniciuscosmome@users.noreply.github.com>

--- src/decorators/account.decorator.ts
+++ src/decorators/account.decorator.ts
@@ -1,4 +1,4 @@
-import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { createParamDecorator, type ExecutionContext } from '@nestjs/common';
 
 export const User = createParamDecorator(
   (data: unknown, ctx: ExecutionContext) => {
